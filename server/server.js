ba4f1b805b53702601dea4106af46aa76719d985
tatianamiyazaki <tatimiyazaki@gmail.com>
2021-09-16 01:08:18 +0000

    Finalizado

--- server/server.js
+++ server/server.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const server = express();
 
+const cors = require("cors");
+
+const Correios = require("node-correios");
+const correio = new Correios();
+
 const porta = 3011;
+
+server.use(cors());
 
 server.listen(porta, () => console.log(`Servidor Rodando na Porta: ${porta}`));
 
-server.get('/', (requisicao, resposta) => {
+server.get('/', (requisicao, resposta) => { 
     console.log('Rota de CEP Encontrada!!!');
     const { cep } = requisicao.query;
     console.log('>> ' + cep);
+
+    correio.consultaCEP({cep:cep})
+    .then( resultado => {
+        resposta.send( resultado );
+        console.log(resultado);
+        console.log("Sucesso na Consulta!!!");
+    })
+    .catch(error =>{
+        console.log("Ocorreu um erro " + error);
+    })
 })
